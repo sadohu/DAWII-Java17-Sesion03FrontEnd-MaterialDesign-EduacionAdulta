fa33ce77f9dd632e77812503c2cdfe6ca0b860f6
sadohu <sadohu26@yahoo.com>
2023-11-19 09:29:31 -0500

    [Add] ConsultaDocente services

--- src/app/services/docente.service.ts
+++ src/app/services/docente.service.ts
@@ -1,10 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Docente } from '../models/docente.model';
 import { AppSettings } from '../app.settings';
 
 const baseUrl =  AppSettings.API_ENDPOINT + "/crudDocente";
+const baseUrlConsulta =  AppSettings.API_ENDPOINT + "/consultaDocente";
 @Injectable({
   providedIn: 'root'
 })
@@ -13,8 +14,8 @@ export class DocenteService {
   constructor(private http:HttpClient) { }
  
   consultaPorNombre(filtro:string):Observable<Docente[]>{
-      return  this.http.get<Docente[]>(baseUrl +"/listaDocentePorNombreLike/"+filtro); 
-  }  
+      return  this.http.get<Docente[]>(baseUrl +"/listaDocentePorNombreLike/"+filtro);
+  }
 
   inserta(obj:Docente):Observable<any>{
       return this.http.post(baseUrl +"/registraDocente", obj);
@@ -26,6 +27,16 @@ export class DocenteService {
 
   elimina(idDocente:number):Observable<any>{
       return this.http.delete(baseUrl + "/eliminaDocente/"+ idDocente);
+  }
+
+  consulta(nombre: string, dni: string, estado: number, idUbigeo: number) : Observable<Docente[]>{
+    const params = new HttpParams()
+      .set("nombre", nombre)
+      .set("dni", dni)
+      .set("estado", estado)
+      .set("idUbigeo", idUbigeo);
+
+    return this.http.get<Docente[]>(baseUrlConsulta + "/consultaDocentePorParametros" + { params });
   }
 
 }
