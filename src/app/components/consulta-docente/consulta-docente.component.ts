4eedd372d80ede845a0103006dfa91d9110c672f
sadohu <sadohu26@yahoo.com>
2023-11-19 09:19:58 -0500

    [Add] Objects to SelectList and Params to query

--- src/app/components/consulta-docente/consulta-docente.component.ts
+++ src/app/components/consulta-docente/consulta-docente.component.ts
@@ -1,5 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { Ubigeo } from 'src/app/models/ubigeo.model';
+import { DocenteService } from 'src/app/services/docente.service';
+import { UbigeoService } from 'src/app/services/ubigeo.service';
 
 @Component({
   selector: 'app-consulta-docente',
@@ -8,10 +11,27 @@ import { MatPaginator } from '@angular/material/paginator';
 })
 export class ConsultaDocenteComponent {
   // Docente List
-  dataSource:any;
+  dataSource: any;
+
   // Paginator Class
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
   // Table Header Display
-  displayedColumns = ["idDocente","nombre","dni","fecha","hora","ubigeo","estado"];
+  displayedColumns = ["idDocente", "nombre", "dni", "fecha", "hora", "ubigeo", "estado"];
+
+  // Ubigeo List for Selects
+  departamentos: string[] = [];
+  provincias: string[] = [];
+  distritos: Ubigeo[] = [];
+
+  // Params to query
+  nombre: string = "";
+  dni: string = "";
+  estado: boolean = true;
+  selDepartamento: string = "-1";
+  selProvincia: string = "-1";
+  selDistrito: number = -1;
+
+  
 
 }
