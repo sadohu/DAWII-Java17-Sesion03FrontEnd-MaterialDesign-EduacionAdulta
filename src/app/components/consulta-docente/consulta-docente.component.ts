1fe577dcc3c8be7eafcb58c59b0c986afd82e27e
sadohu <sadohu26@yahoo.com>
2023-11-19 08:58:58 -0500

    [Add] View ConsultaDocente

--- src/app/components/consulta-docente/consulta-docente.component.ts
+++ src/app/components/consulta-docente/consulta-docente.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-consulta-docente',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./consulta-docente.component.css']
 })
 export class ConsultaDocenteComponent {
+  // Docente List
+  dataSource:any;
+  // Paginator Class
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  // Table Header Display
+  displayedColumns = ["idDocente","nombre","dni","fecha","hora","ubigeo","estado"];
 
 }
