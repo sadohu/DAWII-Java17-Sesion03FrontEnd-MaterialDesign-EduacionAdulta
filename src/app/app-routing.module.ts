c2d45e0b5b1811535c928ff4b65750a78a5d5261
sadohu <sadohu26@yahoo.com>
2023-11-19 08:24:30 -0500

    [Add] ConsultaDocente to Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { CrudDocenteComponent } from './components/crud-docente/crud-docente.component';
+import { ConsultaDocenteComponent } from './components/consulta-docente/consulta-docente.component';
 
 
 const routes: Routes = [
-  {path:"crudDocente", component:CrudDocenteComponent },
+  { path: "crudDocente", component: CrudDocenteComponent },
+  { path: "consultaDocente", component: ConsultaDocenteComponent },
 ];
 
 
