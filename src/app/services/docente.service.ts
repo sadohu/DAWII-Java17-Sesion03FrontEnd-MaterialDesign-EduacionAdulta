52ffbbb9236f80a847980de2a37fb9adb2ab8761
sadohu <sadohu26@yahoo.com>
2023-11-26 11:51:34 -0500

    [Add] Generated report implement

--- src/app/services/docente.service.ts
+++ src/app/services/docente.service.ts
@@ -1,35 +1,36 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Docente } from '../models/docente.model';
 import { AppSettings } from '../app.settings';
+import { map } from 'rxjs/operators';
 
-const baseUrl =  AppSettings.API_ENDPOINT + "/crudDocente";
-const baseUrlConsulta =  AppSettings.API_ENDPOINT + "/consultaDocente";
+const baseUrl = AppSettings.API_ENDPOINT + "/crudDocente";
+const baseUrlConsulta = AppSettings.API_ENDPOINT + "/consultaDocente";
 @Injectable({
   providedIn: 'root'
 })
 export class DocenteService {
- 
-  constructor(private http:HttpClient) { }
- 
-  consultaPorNombre(filtro:string):Observable<Docente[]>{
-      return  this.http.get<Docente[]>(baseUrl +"/listaDocentePorNombreLike/"+filtro);
+
+  constructor(private http: HttpClient) { }
+
+  consultaPorNombre(filtro: string): Observable<Docente[]> {
+    return this.http.get<Docente[]>(baseUrl + "/listaDocentePorNombreLike/" + filtro);
   }
 
-  inserta(obj:Docente):Observable<any>{
-      return this.http.post(baseUrl +"/registraDocente", obj);
+  inserta(obj: Docente): Observable<any> {
+    return this.http.post(baseUrl + "/registraDocente", obj);
   }
 
-  actualiza(obj:Docente):Observable<any>{
-      return this.http.put(baseUrl + "/actualizaDocente", obj);
+  actualiza(obj: Docente): Observable<any> {
+    return this.http.put(baseUrl + "/actualizaDocente", obj);
   }
 
-  elimina(idDocente:number):Observable<any>{
-      return this.http.delete(baseUrl + "/eliminaDocente/"+ idDocente);
+  elimina(idDocente: number): Observable<any> {
+    return this.http.delete(baseUrl + "/eliminaDocente/" + idDocente);
   }
 
-  consulta(nombre: string, dni: string, estado: number, idUbigeo: number) : Observable<Docente[]>{
+  consulta(nombre: string, dni: string, estado: number, idUbigeo: number): Observable<Docente[]> {
     const params = new HttpParams()
       .set("nombre", nombre)
       .set("dni", dni)
@@ -37,6 +38,29 @@ export class DocenteService {
       .set("idUbigeo", idUbigeo);
 
     return this.http.get<Docente[]>(baseUrlConsulta + "/consultaDocentePorParametros", { params });
+  }
+
+  generateDocumentReport(nombre: string,
+    dni: string,
+    estado: number,
+    idUbigeo: number): Observable<any> {
+
+    const params = new HttpParams()
+      .set("nombre", nombre)
+      .set("dni", dni)
+      .set("estado", estado)
+      .set("idUbigeo", idUbigeo);
+
+    let headers = new HttpHeaders();
+    headers.append('Accept', 'application/pdf');
+    let requestOptions: any = { headers: headers, responseType: 'blob' };
+
+    return this.http.post(baseUrlConsulta + "/reporteDocentePdf?nombre=" + nombre + "&dni=" + dni + "&estado=" + estado + "&idUbigeo=" + idUbigeo, '', requestOptions).pipe(map((response) => {
+      return {
+        filename: 'ReporteDocentes.pdf',
+        data: new Blob([response], { type: 'application/pdf' })
+      };
+    }));
   }
 
 }
